7e38881fefd698cc40e4ef48d5f1acff0a913641
21105074 <97309146+Ironman3242@users.noreply.github.com>
2022-01-14 16:16:39 +0530

    Create asg1_output.py

---
+++ asg1_output.py
@@ -0,0 +1,40 @@
+Write a Python program to find average of three numbers entered by the user. 
+
+enter three numbers whose avg is to be calculated: 
+34
+45
+59
+Result:  46.0
+
+Write a python program to compute a person's income tax.
+
+Enter your gross income: 
+1000000
+No. of Dependents: 
+4
+Total income tax you have to pay:  195600.0
+
+Write a python program to store different data type values into a list.
+
+['SID', 'Name', 'Gender', 'Course', 'CGPA']
+[21105243, 'Babandeepsingh', 'M', 'ECE', 9.0]
+[21105235, 'Ansh', 'M', 'ECE', 8.5]
+[21105453, 'jatin', 'F', 'ECE', 8.5]
+
+Write a python program to enter marks of 5 students into a list and display them in sorted manner.
+
+Marks of the student
+Before sorted:  [78, 45, 69, 98, 37]
+After sorting:  [37, 45, 69, 78, 98]
+
+Write a Python program to print a specified list after removing 4th element.
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After removing 4th element
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Remove Black and Pink from the list and replace them with Purple. Do that in one line code.
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After replacing Black and Pink
+['Red', 'Green', 'White', 'purple', 'Yellow']
+
