b40d0fb270992537e3ea7b0026222d6d26409670
21105074 <97309146+Ironman3242@users.noreply.github.com>
2022-01-14 16:04:36 +0530

    Create as1_21105074.py

---
+++ as1_21105074.py
@@ -0,0 +1,104 @@
+#Question-1
+#Taking input from user
+
+print("Write a Python program to find average of three numbers entered by the user.")
+print("")
+
+a = int(input("enter three numbers whose avg is to be calculated: "))
+b = int(input())
+c = int(input())
+
+sum = a + b + c      
+
+avg = sum/3
+
+print("Result: ",avg)
+print("")
+
+#---------------------------------------------------------------------
+#Question-2
+
+print("Write a python program to compute a person's income tax.")
+print("")
+
+#Taking input
+income = float(input("Enter your gross income: "))
+dep = int(input("No. of Dependents: "))          # dep = Dependents
+
+
+ded = 10000                                      # ded = Deduction
+dedPerDep = 3000                                 # dedPerDep = Deduction per dependent
+charge = 1/5                                     # Charge 20%
+
+taxable = income - ded - (dedPerDep * dep)
+
+tax = float()                                    # Converting to float
+tax = taxable * charge
+
+print("Total income tax you have to pay: ",tax)   
+print("")                                    
+
+#------------------------------------------------------------------------
+#Question-3
+
+print("Write a python program to store different data type values into a list.")
+print("")
+
+student = ['SID', 'Name', 'Gender', 'Course', 'CGPA']
+student1 = [21105243, "Babandeepsingh", "M", "ECE", 9.0]
+student2 = [21105235, "Ansh", "M", "ECE", 8.5]
+student3 = [21105453, "jatin", "F", "ECE", 8.5]
+
+
+print(student)
+print(student1)
+print(student2)
+print(student3)
+print("")
+#------------------------------------------------------------------------
+#Question-4
+
+print("Write a python program to enter marks of 5 students into a list and display them in sorted manner.")
+print("")
+
+print("Marks of the student")
+
+marks = [78,45,69,98,37]
+
+print("Before sorted: " ,marks)
+
+marks.sort()
+
+print("After sorting: ", marks)
+print("")
+
+#------------------------------------------------------------------------
+#Question-5
+
+print("Write a Python program to print a specified list after removing 4th element.")
+print("")
+
+
+#Part-1
+color = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
+
+print(color)
+
+print("After removing 4th element")
+
+del color[3]
+
+print(color)
+
+print("Remove Black and Pink from the list and replace them with Purple. Do that in one line code.")
+print("")
+
+#Part-2
+color = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
+
+print(color)
+print("After replacing Black and Pink")
+
+color[3:5] = ['purple']
+
+print(color)
