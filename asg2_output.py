59357e6b0ba8669c36a220625bb59d61b5a6c11c
21105074 <97309146+Ironman3242@users.noreply.github.com>
2022-01-26 16:15:32 +0530

    Create asg2_output.py

---
+++ asg2_output.py
@@ -0,0 +1,49 @@
+The answer of Question 1 is : 
+
+(a)     The length of the given string is : 35
+(b)     The string in reverse would be : egaugnal evitisnes esac a si nohtyP
+(c)     The new string becomes : a case sensitive
+(d)     The replaced substring will be : Python is object oriented language
+(e)     The first occurence of the given substring "a" is at index no. = 10
+(f)     The inputted strings with no white spaces will be "Pythonisacasesensitivelanguage"
+
+
+The answer of Question 2 is :
+
+Enter your name : Babandeep singh
+Enter your SID : 21105074
+Enter the name of your department : ece
+Enter your CGPA : 10
+
+Hey, Babandeep singh Here!
+My SID is 21105074
+I am from ECE department and my CGPA is 10.0
+
+
+The answer of Question 3 is :
+
+a.      a & b = 8
+b.      a | b = 58
+c.      a ^ b = 50
+d.      Left shift both a and b with 2 bits : a = 224, b = 40
+e.      Right shift a with 2 bits and b with 4 bits : a = 14, b = 0
+
+
+The answer of Question 4 is :
+
+Enter first number : 56
+Enter second number : 32
+Enter third number : 105
+The greatest number is 105
+
+
+The solution of Question 5 is :
+
+Enter the string : alptkilopytanameemhablpoik
+Yes
+
+
+The answer of Question 6 is :
+
+Enter the dimensions to check whether triangle possible or not : 5 4 11
+No
