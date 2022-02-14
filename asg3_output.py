81834c50fea1eb3ac0169251dadbb2b8f681e745
21105074 <97309146+Ironman3242@users.noreply.github.com>
2022-02-14 23:00:52 +0530

    Create asg3_output.py

---
+++ asg3_output.py
@@ -0,0 +1,132 @@
+
+Question 1
+Please give an input- babandeep
+Total occurances
+b-2
+a-2
+n-1
+d-1
+e-2
+p-1
+
+Question 1
+Please give an input-BABANDEEP SINGH babandeep singh bABANDEEP babandeep 
+Total occurances
+babandeep-4
+singh-2
+
+
+Question 2
+Please enter Day- 31
+Please enter Month- 12
+Please enter Year- 2021
+Next Date is: 1/1/2022
+
+
+Question 2
+Please enter Day- 29
+Please enter Month- 02
+Please enter Year- 2020
+Next Date is: 1/3/2020
+  
+Question 3
+Plz enter Y to enter data- Y
+Plz give an integer input- 2
+Plz enter Y to enter data- Y
+Plz give an integer input- 6
+Plz enter Y to enter data- Y
+Plz give an integer input- 7
+Plz enter Y to enter data- Y
+Plz give an integer input- 33
+Plz enter Y to enter data- N
+[2, 6, 7, 33]
+[(2, 4), (6, 36), (7, 49), (33, 1089)]
+
+
+Question 4
+Enter Grade Points- 9
+Your Grade is A and Excellent performance 
+
+
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A 
+ 
+
+Question 6
+Please Enter SID of Student- 21105074
+Please enter the name of the Student- babandeep singh
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105277
+Please enter the name of the Student- rahul Goyal
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105183
+Please enter the name of the Student- anshika rawat
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105248
+Please enter the name of the Student- aneesh deshmukh
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105145
+Please enter the name o f the Student- sarthak thukral
+If you want to enter more details press Y/N- N
+Part a
+21105074 is babandeep singh
+21105277 is rahul Goyal
+21105183 is anshika rawat
+21105248 is aneesh deshmukh
+21105145 is sarthak thukral
+
+Part b
+value sorted dictionary is [aneesh deshmukh,anshika rawat, babandeep singh, rahul goyal, sarhak thukral]
+
+Part c
+Keys sorted dictionary is [21105074, 21105145, 21105183, 21105248, 21105277]
+
+Part D
+Please enter the student's SID whose detail you need- 21105074
+Name of the required student is babandeep singh
+
+
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 3
+Fibonnaci sequence with 3 terms
+0
+1
+1
+Average of total 3 terms of sequence is 1
+END
+
+
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 11
+Fibonnaci sequence with 11 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+Average of total 11 terms of sequence is 13
+END
+
+
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7} 
