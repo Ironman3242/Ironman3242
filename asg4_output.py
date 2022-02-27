91e23a0a0c282d25a59edafac8c68beeda636865
21105074 <97309146+Ironman3242@users.noreply.github.com>
2022-02-27 20:43:03 +0530

    Create asg4_output.py

---
+++ asg4_output.py
@@ -0,0 +1,76 @@
+ Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+QUESTION 2.................................. 
+ 
+Enter the number of rows in Pascal's Triangle: 7
+
+Using recursions:
+
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+Using loops:
+
+            1   
+          1   1              
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+QUESTION 3....................................
+
+ 
+ 
+Enter the first integer: 3
+Enter the first integer: 2
+Remainder is:  1
+Quotient is:  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+QUESTION 4.............................................
+ 
+ 
+Object created
+The name of the student is babandeep singh and SID is 21105074.
+Object destroyed
+
+QUESTION 5..............................................
+ 
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+QUESTION 6...............................................
+ 
+ 
+Enter the first word: tub
+
+Enter a new meaningful word to test your friendship: but
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
